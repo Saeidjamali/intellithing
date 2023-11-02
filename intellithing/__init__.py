f02b6ad75c848dc2726f18bb27db2ac953467378
Saeidjamali <86665070+Saeidjamali@users.noreply.github.com>
2023-11-02 12:01:03 +0000

    Update __init__.py

--- intellithing/__init__.py
+++ intellithing/__init__.py
@@ -2,4 +2,5 @@ from .layer_freezer import LayerFreezer
 from .lr_finder import LearningRateFinderCallback
 from .hyper_tuner import HyperparameterTuner
 from .autotrainer import AutoTrainer
+from .t5customdataset import T5RegressionDataset, T5ClassificationDataset, T5QADataset, T5TextGenerationDataset, T5SummarizationDataset, T5NERDataset, T5TranslationDataset
 
